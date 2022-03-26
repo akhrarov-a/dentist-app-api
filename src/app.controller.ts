50a85c01b0b8be89caadf4ff3bcd6a5e32540e06
akhrarov-a <asad.lion06@gmail.com>
2022-03-26 17:09:04 +0500

    #initial configs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * App Controller
+ */
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
+  /**
+   * Get hello
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * App Service
+ */
 @Injectable()
 export class AppService {
+  /**
+   * Get hello
+   */
   getHello(): string {
     return 'Hello World!';
   }
